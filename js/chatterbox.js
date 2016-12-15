4cafbd6644740260302199315b2bd62e09514326
Margo Lanier <lanier.margo@gmail.com>
2016-12-15 16:06:08 -0500

    adding media queries

--- js/chatterbox.js
+++ js/chatterbox.js
@@ -1,17 +1,19 @@
 function init() {
+	
+	// AJAX request for chats
     let request = new XMLHttpRequest();
     request.open('GET', 'http://api.queencityiron.com/chats');
     request.addEventListener('load', function() {
         let response = JSON.parse(request.responseText);
         for(let i =0; i < response.chats.length; i++) {
             let message = response.chats[i];
+			
             showChats(message);
-            console.log(message);
+            //console.log(message);
         }
     });
-
     request.send();
-
+	
     let button = document.querySelector('#send');
     button.addEventListener('click', sendMessage);
 }
@@ -19,27 +21,32 @@ function init() {
 function showChats(chat) {
     let parent = document.querySelector('ul');
     
+	// Create list item elements
     let item = document.createElement('li');
     parent.appendChild(item);
 
-    let user = document.createElement('p');
-    user.textContent = ('From: ' + chat.from);
-    item.appendChild(user);
+		let user = document.createElement('p');
+		user.textContent = ('From: ' + chat.from);
+		item.appendChild(user);
 
-    let message = document.createElement('p');
+		let message = document.createElement('p');
 
-    message.textContent = ('Message: ' + chat.message);
-    item.appendChild(message);
+		message.textContent = ('Message: ' + chat.message);
+		item.appendChild(message);
 }
 
 
 function sendMessage (){
-    console.log("send message");
-    let textBox = document.querySelector('#user-message');
+    
+	// Get user input
+	let textBox = document.querySelector('#user-message');
     let userInput = textBox.value;
-    //AJAX request here
+	textBox.value = '';
+	
+	// Post message to chats
+    // AJAX request here
     console.log(userInput);
-    textBox.value = '';
+    
 }
 
 
