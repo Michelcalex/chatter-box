56f677099b985d58708c0832aeaa633a20763c4d
Lex Michel <michelca@Lexs-MacBook-Pro.local>
2016-12-15 14:41:31 -0500

    created showChat function

--- js/chatterbox.js
+++ js/chatterbox.js
@@ -5,12 +5,27 @@ function init() {
         let response = JSON.parse(request.responseText);
         for(let i =0; i < response.chats.length; i++) {
             let message = response.chats[i];
-            //showChats(message);
+            showChats(message);
             console.log(message);
         }
     });
 
     request.send();
+}
+
+function showChats(chat) {
+    let parent = document.querySelector('ul');
+    
+    let item = document.createElement('li');
+    parent.appendChild(item);
+
+    let user = document.createElement('p');
+    user.textContent = ('From: ' + chat.from);
+    item.appendChild(user);
+
+    let message = document.createElement('p');
+    message.textContent = ('Message: ' + chat.message);
+    item.appendChild(message);
 }
 
 window.addEventListener('load', init);
